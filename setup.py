dd8b798ceddc7973d7e3a48b0ad777f216d0d1ff
wDove1 <69383952+wDove1@users.noreply.github.com>
2020-11-27 20:52:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 setup(
-    name=’CameraTrackingPython’,
+    name='CameraTrackingPython',
     packages=find_packages(),
-    version=’0.0.0',
-    description=’Used to create a tracking tripod head’,
-    author=’William Dove’,
-    license=’Apache 2.0’,
+    version='0.0.0',
+    description='Used to create a tracking tripod head',
+    author='William Dove',
+    license='Apache 2.0',
 )
