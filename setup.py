2bdaa18998abc19462df84e357244b040354d715
wDove1 <69383952+wDove1@users.noreply.github.com>
2020-11-27 21:16:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,15 @@ setup(
     packages=find_packages(),
     version='0.0.0',
     description='Used to create a tracking tripod head',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author='William Dove',
+    author_email='williamtdove@gmail.com',
+    url='https://github.com/wDove1/Camera-Tracking-Python',
     license='Apache 2.0',
+    install_requires=[MotorControl,MeasureMotorSpecs],
+    classifiers=["Programming language :: Python :: 3",
+                 "License :: OSI Approved :: Apache License 2.0",
+                 "Operating System :: OS Independent"],
+    python_requires='>=3.7'
 )
