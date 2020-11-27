4bdc5747d5e30ca3a3165ab8ef4c19bb8d44125e
wDove1 <69383952+wDove1@users.noreply.github.com>
2020-11-27 17:36:35 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name=’CameraTrackingPython’,
+    packages=find_packages(),
+    version=’0.0.0',
+    description=’Used to create a tracking tripod head’,
+    author=’William Dove’,
+    license=’Apache 2.0’,
+)
